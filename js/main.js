6787a2c2a446042aa6b373ade9b2396f7650db54
marin-mar <72693639+marin-mar@users.noreply.github.com>
2021-09-02 10:08:49 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -65,3 +65,16 @@ window.onscroll = function () {
     header.classList.remove("header--sticky");
   }
 };
+
+const stars = document.querySelectorAll(".stars__star");
+stars.forEach(star => {
+  star.addEventListener("click", function (e) {
+    const fillColor = e.target.getAttribute("fill");
+
+    if (fillColor == "#666666") {
+      e.target.setAttribute("fill", "#f3cd03");
+    } else {
+      e.target.setAttribute("fill", "#666666");
+    }
+  });
+});
