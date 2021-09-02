8cfb71590f5cf4b7a3e65acc162f11797cdbe4b8
marin-mar <72693639+marin-mar@users.noreply.github.com>
2021-09-02 19:38:24 +0300

    Updates

--- js/main.js
+++ js/main.js
@@ -27,7 +27,7 @@ if (menuIcon) {
 } // smooth scroll
 
 
-const menulinks = document.querySelectorAll(".menu__item > .menu__link[data-scrollTo]");
+const menulinks = document.querySelectorAll("[data-scrollTo]");
 
 if (menulinks.length > 0) {
   menulinks.forEach(menuLink => {
@@ -76,5 +76,16 @@ stars.forEach(star => {
     } else {
       e.target.setAttribute("fill", "#666666");
     }
+  });
+});
+const nonexistentLinks = document.querySelectorAll('a[href="#"]');
+nonexistentLinks.forEach(nonexistentLink => {
+  nonexistentLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log("Popup404");
+    alert(`
+		This is a popup 404
+		Thank you for your click
+		This link is under development`);
   });
 });
