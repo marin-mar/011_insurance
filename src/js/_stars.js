c79a29ac19889e62de38d4431061da10e7d52f8c
marin-mar <72693639+marin-mar@users.noreply.github.com>
2021-09-02 10:04:53 +0300

    add reviews

---
+++ src/js/_stars.js
@@ -0,0 +1,13 @@
+const stars = document.querySelectorAll(".stars__star");
+
+stars.forEach((star) => {
+  star.addEventListener("click", function (e) {
+    const fillColor = e.target.getAttribute("fill");
+
+    if (fillColor == "#666666") {
+      e.target.setAttribute("fill", "#f3cd03");
+    } else {
+      e.target.setAttribute("fill", "#666666");
+    }
+  });
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,3 @@
 @@include('_webpcss.js')
 @@include('_header.js')
+@@include('_stars.js')
